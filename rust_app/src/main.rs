653c9094e4c8698e44208a432b8c075cf6f85779
Pablo Felitti <pablofelitti@Pablos-MBP-14.fibertel.com.ar>
2023-06-21 00:48:24 -0300

    added ssm call

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,15 +1,27 @@
-use aws_lambda_events::event::sqs::SqsEvent;
+use aws_lambda_events::sqs::SqsEvent;
+use aws_sdk_ssm::Client;
+use aws_sdk_ssm::config::Region;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
-    println!("Hello world!!");
+    println!("Hello world!! {:?}", event);
+
+    let region_provider = Region::new("us-east-1");
+
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    show_parameters(&client).await;
+
     Ok(())
+}
+
+async fn show_parameters(client: &Client) -> () {
+    let resp = client.describe_parameters().send().await.unwrap();
+
+    for param in resp.parameters().unwrap().iter() {
+        println!("  {}", param.name().unwrap_or_default());
+    }
 }
 
 #[tokio::main]
