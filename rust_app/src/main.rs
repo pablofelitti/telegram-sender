7e020e2ae2dd8097ba96fba42729b883b968fb33
Pablo Felitti <pablofelitti@gmail.com>
2023-06-27 21:00:00 -0300

    change api

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -11,33 +11,51 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&shared_config);
 
-    show_parameters(&client, &event).await;
+    let res = show_parameters(&client, &event).await;
+
+    println!("Parameters: {:?}", res);
 
     Ok(())
 }
 
-async fn show_parameters(client: &Client, event: &LambdaEvent<SqsEvent>) -> () {
+async fn show_parameters(client: &Client, event: &LambdaEvent<SqsEvent>) -> (String, String) {
     let first_records = event.payload.records.get(0);
     match first_records {
-        None => {println!("No records found for event")}
+        None => { panic!("No records found for event") }
         Some(r) => {
-            let env = r.message_attributes.get("EnvironmentId");
-            match env {
-                None => {println!("No message attribute for the record")}
+            match r.message_attributes.get("EnvironmentId") {
+                None => { panic!("No message attribute for the record") }
                 Some(e) => {
-                    let path = "/telegram-ids/".to_owned() + e.string_value.clone().unwrap().as_str();
-                    let resp = client.get_parameters_by_path().path(path).send().await.unwrap();
-
-                    for param in resp.parameters().unwrap().iter() {
-                        match param.name() {
-                            None => {}
-                            Some(name) => { println!("  {name}") }
-                        }
-                    }
+                    let params = load_all_params(client, e.string_value.clone().unwrap().as_str()).await;
+                    return params
                 }
             }
         }
     }
+    panic!()
+}
+
+async fn load_all_params(client: &Client, env: &str) -> (String, String) {
+    let path = "/telegram-ids/".to_owned() + env;
+    println!("PATH: {}", path);
+    let resp = client.get_parameters_by_path().path(path).send().await.unwrap();
+
+    let name;
+    let value;
+
+    for param in resp.parameters().unwrap().iter() {
+        match param.name() {
+            None => { panic!() }
+            Some(_name) => name = _name
+        }
+        match param.value() {
+            None => { panic!() }
+            Some(_value) => value = _value
+        }
+
+        return (name.to_string(), value.to_string());
+    }
+    panic!()
 }
 
 #[tokio::main]
