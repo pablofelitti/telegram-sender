8d40e79d9ce82dbbae82443157b0a7ce420f2830
Pablo Felitti <pablofelitti@gmail.com>
2023-07-02 20:23:50 -0300

    all values

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,4 +1,4 @@
-use aws_lambda_events::sqs::{SqsEvent, SqsMessage, SqsMessageAttribute};
+use aws_lambda_events::sqs::{SqsEvent, SqsMessage};
 use aws_sdk_ssm::Client;
 use aws_sdk_ssm::config::Region;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
@@ -11,37 +11,37 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&shared_config);
 
-    let res = show_parameters(&client, &event).await;
+    let res = show_parameters(&client, &event.payload.records.get(0)).await;
 
     println!("Parameters: {:?}", res);
 
     Ok(())
 }
 
-async fn show_parameters(client: &Client, event: &LambdaEvent<SqsEvent>) -> (String, String) {
-    let first_records = event.payload.records.get(0);
-    match first_records {
+async fn show_parameters(client: &Client, msg: &Option<&SqsMessage>) {
+    match *msg {
         None => { panic!("No records found for event") }
         Some(r) => {
             match r.message_attributes.get("EnvironmentId") {
                 None => { panic!("No message attribute for the record") }
                 Some(e) => {
                     let params = load_all_params(client, e.string_value.clone().unwrap().as_str()).await;
-                    return params
+                    println!("{} params have been found", params.len());
                 }
             }
         }
     }
-    panic!()
 }
 
-async fn load_all_params(client: &Client, env: &str) -> (String, String) {
+async fn load_all_params(client: &Client, env: &str) -> Vec<(String, String)> {
     let path = "/telegram-ids/".to_owned() + env;
     println!("PATH: {}", path);
     let resp = client.get_parameters_by_path().path(path).send().await.unwrap();
 
-    let name;
-    let value;
+    let mut name;
+    let mut value;
+
+    let mut v: Vec<(String, String)> = Vec::new();
 
     for param in resp.parameters().unwrap().iter() {
         match param.name() {
@@ -53,9 +53,9 @@ async fn load_all_params(client: &Client, env: &str) -> (String, String) {
             Some(_value) => value = _value
         }
 
-        return (name.to_string(), value.to_string());
+        v.push((name.to_string(), value.to_string()));
     }
-    panic!()
+    return v;
 }
 
 #[tokio::main]
