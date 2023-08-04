8b9b075d4d6e5d1d8574104bd7cf6164ccf7f3bc
Pablo Felitti <pablofelitti@gmail.com>
2023-08-04 00:29:31 -0300

    refactor

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -4,7 +4,7 @@ use aws_sdk_ssm::Client;
 use aws_sdk_ssm::config::Region;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Box<dyn std::error::Error>> {
     let region_provider = Region::new("us-east-1");
 
     let shared_config = aws_config::from_env().region(region_provider).load().await;
@@ -17,26 +17,20 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
     let environment_id = _msg.message_attributes["EnvironmentId"].string_value.as_ref().expect("Environment id not present");
 
     let url = format!("/telegram-ids/{environment_id}/token");
-    let token = get_parameter(&client, _msg, &url).await.unwrap();
+    let token = get_parameter(&client, _msg, &url).await.expect("Token not found");
 
     let url = format!("/telegram-ids/{environment_id}/{channel}");
-    let channel_id = get_parameter(&client, _msg, &url).await.unwrap();
+    let channel_id = get_parameter(&client, _msg, &url).await.expect("Channel id not found");
 
     let mut map = HashMap::new();
     map.insert("chat_id", channel_id.as_str());
-    let text = match &_msg.body {
-        None => { "" }
-        Some(txt) => { &txt }
-    };
-    map.insert("text", text);
+    let text = _msg.body.as_ref().expect("Text expected in the message");
+    map.insert("text", &text);
 
     let client = reqwest::Client::new();
-    let res = client.post(format!("https://api.telegram.org/bot{token}/sendMessage"))
+    client.post(format!("https://api.telegram.org/bot{token}/sendMessage"))
         .json(&map)
         .send().await?;
-
-    println!("FINISHHHHHHHHHH {:?}", res);
-
     Ok(())
 }
 
