2aa9106d6fee7c74754aa839c1b107d661190414
Pablo Felitti <pablofelitti@gmail.com>
2023-08-08 19:48:40 -0300

    refactor with struct

--- rust_app/src/main.rs
+++ rust_app/src/main.rs
@@ -1,8 +1,27 @@
 use std::collections::HashMap;
-use aws_lambda_events::sqs::SqsEvent;
+use aws_lambda_events::sqs::{SqsEvent, SqsMessage};
 use aws_sdk_ssm::Client;
 use aws_sdk_ssm::config::Region;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+
+struct Props<'a> {
+    channel: &'a String,
+    environment: &'a String,
+    text: &'a String,
+}
+
+impl Props<'_> {
+    fn new(_msg: &SqsMessage) -> Props<'_> {
+        let channel = _msg.message_attributes["Channel"].string_value.as_ref().expect("Channel name not present");
+        let environment = _msg.message_attributes["EnvironmentId"].string_value.as_ref().expect("Environment id not present");
+        let text = _msg.body.as_ref().expect("Text expected in the message");
+        Props {
+            channel,
+            environment,
+            text,
+        }
+    }
+}
 
 async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Box<dyn std::error::Error>> {
     let region_provider = Region::new("us-east-1");
@@ -13,19 +32,17 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Box<dyn st
     //TODO process every message despite lambda being configured to trigger each msg
     let _msg = event.payload.records.get(0).expect("Message not present");
 
-    let channel = _msg.message_attributes["Channel"].string_value.as_ref().expect("Channel name not present");
-    let environment = _msg.message_attributes["EnvironmentId"].string_value.as_ref().expect("Environment id not present");
+    let props = Props::new(_msg);
 
-    let url = format!("/telegram-ids/{environment}/token");
-    let token = get_parameter(&client, environment, &url).await.expect("Token not found");
+    let url = format!("/telegram-ids/{0}/token", props.environment);
+    let token = get_parameter(&client, props.environment, &url).await.expect("Token not found");
 
-    let url = format!("/telegram-ids/{environment}/{channel}");
-    let channel_id = get_parameter(&client, environment, &url).await.expect("Channel id not found");
+    let url = format!("/telegram-ids/{0}/{1}", props.environment, props.channel);
+    let channel_id = get_parameter(&client, props.environment, &url).await.expect("Channel id not found");
 
     let mut map = HashMap::new();
     map.insert("chat_id", channel_id.as_str());
-    let text = _msg.body.as_ref().expect("Text expected in the message");
-    map.insert("text", &text);
+    map.insert("text", &props.text);
 
     let client = reqwest::Client::new();
     client.post(format!("https://api.telegram.org/bot{token}/sendMessage"))
